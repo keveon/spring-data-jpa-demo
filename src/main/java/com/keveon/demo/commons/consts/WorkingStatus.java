678af6f91229bc2ce9a6f4a3bbdc622a72a459dc
Melora <melora@melora.moe>
2026-04-23 20:41:30 +0800

    [verified] feat: create GitHub Pages landing page

--- src/main/java/com/keveon/demo/commons/consts/WorkingStatus.java
+++ src/main/java/com/keveon/demo/commons/consts/WorkingStatus.java
@@ -3,8 +3,6 @@ package com.keveon.demo.commons.consts;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 import org.jetbrains.annotations.NotNull;
 
@@ -13,9 +11,7 @@ import org.jetbrains.annotations.NotNull;
  * @version 1.0.0
  * @since 1.0.0
  */
-@Getter
 @ToString
-@RequiredArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum WorkingStatus {
     /**
@@ -36,6 +32,19 @@ public enum WorkingStatus {
 
     private final Integer code;
     private final String desc;
+
+    WorkingStatus(Integer code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
 
     @NotNull
     @JsonCreator
